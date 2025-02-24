20e3d330bf90bef0c5d472c1d7ced1789bbb80a6
Rinat Akhmetdiyanov <ahmetdianovv@yandex.ru>
2025-02-24 23:51:33 +0300

    fix URL concatenation

--- url.py
+++ url.py
@@ -1,4 +1,4 @@
-MAIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site/'
-REGISTER_PAGE_URL = 'https://stellarburgers.nomoreparties.site/register'
-FORGOT_PASSWORD_URL = 'https://stellarburgers.nomoreparties.site/forgot-password'
-LOGIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site/login'
+MAIN_PAGE_URL = 'https://stellarburgers.nomoreparties.site'
+REGISTER_PAGE_URL = f'{MAIN_PAGE_URL}/register'
+FORGOT_PASSWORD_URL = f'{MAIN_PAGE_URL}/forgot-password'
+LOGIN_PAGE_URL = f'{MAIN_PAGE_URL}/login'
